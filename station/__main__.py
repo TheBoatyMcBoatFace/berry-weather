a0a8d737126f1798328d6e994f5192a58486388e
Bentley Hensel <bentleyhensel@gmail.com>
2025-01-24 19:21:22 -0500

    imnprot
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

--- station/__main__.py
+++ station/__main__.py
@@ -1,7 +1,7 @@
 # station/__main__.py
 from .config import load_config
 from .sensors import setup_temperature_sensors, get_temperatures, get_wifi_signal_strength
-from .setup import setup_gpio
+from .gpio import setup_gpio
 import RPi.GPIO as GPIO
 
 def main():
